131ff0b46bde07c71c3791364ba7f2092605d431
mateopautassoPC <mateopautasso.mg@gmail.com>
2023-03-07 15:28:53 -0300

    clase 'NewCard' creada. Falta agregar funcionalidad para agregar el check a la tarea

--- js/tasks.js
+++ js/tasks.js
@@ -1,16 +1,10 @@
-const cardContainer = document.querySelector('.card-container')
+const cardContainer = document.querySelector('.card-container');
+const cardAgregarCardContainer = document.querySelector('.card-container__agregar-card');
+const cardTareasContainer = document.querySelector('.card-container__cards');
 
-class nuevaTarea {
-    constructor(name, pomos) {
-        this.name = name
-        this.pomos = pomos
-    }
-}
-const listaTareas = [];
-
+// Card para agregar Cards
 let cardAgregarTarea = `
-    <div class="card-container__agregar-card">
-        <div class="card-container__card--agregar card">
+        <div class="card-container__card--agregar">
             <img src="./assets/icons8-add-24.png" alt="">
             <p>Agregar</p>
         </div>
@@ -27,31 +21,63 @@ let cardAgregarTarea = `
                 <button class="agregar-card__btn-guardar">Guardar</button>
             </div>
         </div>
-    </div>
 `;
-listaTareas.push(cardAgregarTarea);
-cardContainer.innerHTML += listaTareas[listaTareas.length - 1];
+cardAgregarCardContainer.innerHTML = cardAgregarTarea;
 
-function guardarNuevaTarea() {
-
-}
-
-
-// Eventos cards
+// Eventos agregar cards
 const agregarTareaElemento = document.querySelector('.card-container__card--agregar');
-const lineCheckTarea = document.querySelector('.card__title-tarea span');
-const btnCheckTarea = document.querySelector('.card__check');
 const agregarTareaMenu = document.querySelector('.agregar-card');
 const btnTareaMenuCancelar = document.querySelector('.agregar-card__btn-cancelar')
 const btnTareaMenuGuardar = document.querySelector('.agregar-card__btn-guardar')
 
-btnCheckTarea.addEventListener('click', ()=>{
-    lineCheckTarea.classList.toggle('tarea-checked');
-    btnCheckTarea.classList.toggle('btn-tarea-checked')
-})
 agregarTareaElemento.addEventListener('click', ()=>{
     agregarTareaMenu.classList.add('agregar-tarea-active');
 })
 btnTareaMenuCancelar.addEventListener('click', ()=>{
     agregarTareaMenu.classList.remove('agregar-tarea-active');
 })
+
+
+
+// Nuevas Cards
+
+listaTareas = [];
+
+class NewCard {
+    constructor(name, pomos) {
+        this.name = name;
+        this.pomos = pomos;
+        this.checked = false;
+        this.body = `
+        <div class="card__container">
+            <div class="card__left">
+                <div class="card__check"></div>
+                <p class="card__title-tarea">${name}<span></span></p>
+            </div>
+            <div class="card__right">
+                <div>
+                    <p>0<span>/</span></p><p>${pomos}</p>
+                </div>
+                <img src="./assets/icons8-more-48-grey.png" alt="">
+            </div>
+        </div>`
+    }
+    checkTarea() {
+        this.checked = true;
+    }
+}
+
+function guardarNuevaTarea() {
+    let nameTarea = document.querySelector('.agregar-card__name-tarea').value;
+    let pomosTarea = document.querySelector('.agregar-card__input').value;
+
+    let newTarea = new NewCard(nameTarea, pomosTarea);
+    listaTareas.unshift(newTarea);
+    cardTareasContainer.innerHTML += listaTareas[0].body
+
+    agregarTareaMenu.classList.remove('agregar-tarea-active');
+}
+btnTareaMenuGuardar.addEventListener('click', guardarNuevaTarea)
+
+
+
